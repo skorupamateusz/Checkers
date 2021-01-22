60b8ea5f2045109af975dac79607ce64335a1d0f
Wojtek <swswojtek@gmail.com>
2021-01-22 22:54:25 +0100

    new changes

--- src/UserServices/Rank.java
+++ src/UserServices/Rank.java
@@ -15,23 +15,25 @@ public class Rank extends JPanel {
     private final URL iconurl;
     private final Image iconimage;
     private JLabel label;
+    private JPanel tabpanel = new JPanel();
+    private final Color kolor = new Color(207, 72, 62);
 
     public Rank() throws Exception {
-        //super("Ranking");
 
+        this.setBackground(kolor);
         label = new JLabel();
         label.setText("RANKING GRACZY");
-        label.setFont(new Font("Arial", Font.ROMAN_BASELINE, 25));
+        label.setFont(new Font("Arial", Font.BOLD, 30));
         JLabel label2 = new JLabel();
         label2.setText("Twoje miejsce w rankingu to: ");
         label2.setFont(new Font("Arial", Font.ROMAN_BASELINE, 15));
 
         JPanel jp = new JPanel();
-        jp.setBackground(Color.gray);
+        jp.setBackground(kolor);
         jp.add(label);
         JPanel jp2 = new JPanel();
         jp2.setBackground(Color.gray);
-        jp2.add(label2);
+        jp2.add(label2, BorderLayout.PAGE_END);
 
         iconurl = new URL("https://image.shutterstock.com/image-vector/podium-icon-260nw-325772819.jpg");
         iconimage = ImageIO.read(iconurl);
@@ -44,14 +46,17 @@ public class Rank extends JPanel {
         columns.addElement("Punkty");
         try {
             table = new JTable(db.fill(), columns);
-            table.setGridColor(Color.orange );
-
+            table.setGridColor(Color.gray );
+            table.setBackground(Color.gray);
+            table.setFont(new Font("Arial", Font.CENTER_BASELINE, 15));
             //this.setLayout(new BorderLayout());
             setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
             this.add(jp, BorderLayout.PAGE_START);
-            //this.add(jp2,  BorderLayout.PAGE_END);
+            this.add(new JSeparator(SwingConstants.VERTICAL));
+            this.add(Box.createHorizontalStrut(60));
             this.add(table.getTableHeader(), BorderLayout.NORTH);
             this.add(table, BorderLayout.CENTER);
+            this.add(jp2,  BorderLayout.PAGE_END);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -64,6 +69,8 @@ public class Rank extends JPanel {
         this.add(scrollPane, BorderLayout.CENTER);
         this.setSize(300, 177);
         this.setVisible(true);
+        this.setBorder(BorderFactory.createEmptyBorder(50,15,50,15));
+
 //        this.setResizable(false);
         //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
